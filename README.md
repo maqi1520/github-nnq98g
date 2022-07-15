1688e937c3c2d95614cff8ec71a3374c7b40960a
maqi1520 <maqi1520@163.com>
2022-07-15 16:17:47 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# github-nnq98g-8iyq9g
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-nnq98g-8iyq9g)
---
+++ app2/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div
+      style={{
+        margin: '10px',
+        padding: '10px',
+        textAlign: 'center',
+        backgroundColor: 'cyan',
+      }}
+    >
+      <h1>修改 App 2 中的代码，并保存</h1>
+    </div>
+  );
+};
+
+export default App;
